45940ab341953cadc71da903c1372194d895377b
MastaPA <vvdegtyarev@gmail.com>
2016-04-01 15:29:56 +0300

    baseEquipment commit

---
+++ SmartHouse/SmartHouse/BaseEquipment.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse
+{
+    class BaseEquipment
+    {
+        public BaseEquipment()
+        {
+            setState(0);
+
+        }
+
+        public void setState(int a)
+        {
+            if (a == 0)
+                state = false;
+            else
+                state = true;
+        }
+
+        public bool getState() { return state; }
+
+        public void setName()
+        {
+            string s;
+            do
+            {
+                Console.WriteLine("Введите название - ");
+                s = Console.ReadLine();
+            } while (s == null);
+
+            name = s;
+        } 
+
+        public void Status ()
+        {
+            Console.WriteLine("Имя объекта - " + name + "\nСостояние - " + ((state == true) ? "Вкл" : "Выкл"));
+        }
+
+        private string name;
+        private bool state;
+    }
+}
